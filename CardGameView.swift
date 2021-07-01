c3ed8bd20fb135dc22dfe42d56f7aa1d10ee1f26
Deonatan Dinata <69316014+Deonatan@users.noreply.github.com>
2021-07-01 17:47:35 +0800

    Update CardGameView.swift

--- CardGameView.swift
+++ CardGameView.swift
@@ -44,8 +44,14 @@ struct ContentView: View {
                     cpuCard = "card"+String(cpuRand)
                     
                     // Update Score
-                    playerScore += 1
-                    cpuScore += 1
+                    
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    }
+                    
+                    else if cpuRand > playerRand{
+                        cpuScore += 1
+                    }
                     
                     
                 }, label: {
